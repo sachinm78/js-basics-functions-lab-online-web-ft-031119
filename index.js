e186f8d9aaf490184541a80b8ba19e5d3f16a7a1
Sachin Mahajan <sachinm78@gmail.com>
2019-06-10 23:07:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks) {
+  //returns the number of blocks given a value
+  if (blocks <= 42) {
+    return 42 - blocks;
+  } else {
+    return blocks - 42;
+  }
+}
+ 
+function distanceFromHqInFeet(blocks) {
+  // calls distanceFromHqInBlocks from inside the distanceFromHqInFeet function, passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+  //the return value of distanceFromHqInBlocks can then be used to calculate feet
+  return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet (start, destination) {
+  if (start < destination) {
+    return (destination - start) * 264;
+  } else {
+    return (start - destination) * 264;
+  }
+}
+
+function calculatesFarePrice (start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
