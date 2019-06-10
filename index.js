4006ce6beeb32806379f1d99e04dc8c8193f367d
Sachin Mahajan <sachinm78@gmail.com>
2019-06-10 23:01:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function distanceFromHqInBlocks(blocks) {
   }
 }
  
-function distanceFromHqInFeet(blocksTravelled) {
+function distanceFromHqInFeet(blocks) {
   // calls distanceFromHqInBlocks from inside the distanceFromHqInFeet function, passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
   //the return value of distanceFromHqInBlocks can then be used to calculate feet
   blocksTravelled = distanceFromHqInBlocks(blocks)
